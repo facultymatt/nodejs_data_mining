999636f35c4f41bb0a866b16077369199b7581f9
Matt Miller <matt@facultycreative.com>
2014-05-13 23:50:41 -0400

    added call after to tests and util

--- test/server/ofxaddons/githubUtil.js
+++ test/server/ofxaddons/githubUtil.js
@@ -3,12 +3,14 @@
 var expect = require('expect.js');
 var githubUtil = require('../../../lib/ofxaddons/githubUtil.js');
 var gitParse = githubUtil.parseRepoContentsAsync;
+var callAfter = githubUtil.callAfterGivenTime;
+
 
 describe('Parse repo contents', function() {
 
   var files;
 
-  it('finds `addon_config.mk`', function() {
+  it('finds `addon_config.mk`', function(done) {
 
     files = [{
       name: 'addon_config.mk'
@@ -16,11 +18,12 @@ describe('Parse repo contents', function() {
 
     gitParse(null, files, function(result) {
       expect(result.hasMakefile).to.be(true);
+      done();
     });
 
   });
 
-  it('finds `addon.make`', function() {
+  it('finds `addon.make`', function(done) {
 
     files = [{
       name: 'addon.make'
@@ -28,11 +31,12 @@ describe('Parse repo contents', function() {
 
     gitParse(null, files, function(result) {
       expect(result.hasMakefile).to.be(true);
+      done();
     });
 
   });
 
-  it('finds `readme.md`', function() {
+  it('finds `readme.md`', function(done) {
 
     files = [{
       name: 'readme.md'
@@ -40,11 +44,12 @@ describe('Parse repo contents', function() {
 
     gitParse(null, files, function(result) {
       expect(result.hasReadme).to.be(true);
+      done();
     });
 
   });
 
-  it('finds `README.md`', function() {
+  it('finds `README.md`', function(done) {
 
     files = [{
       name: 'README.md'
@@ -52,11 +57,12 @@ describe('Parse repo contents', function() {
 
     gitParse(null, files, function(result) {
       expect(result.hasReadme).to.be(true);
+      done();
     });
 
   });
 
-  it('counts folders/ files starting with `example` as examples', function() {
+  it('counts folders/ files starting with `example` as examples', function(done) {
 
     files = [{
       name: 'example-1'
@@ -70,11 +76,12 @@ describe('Parse repo contents', function() {
 
     gitParse(null, files, function(result) {
       expect(result.numExamples).to.be(4);
+      done();
     });
 
   });
 
-  it('finds `changelog.md` and `CHANGELOG.md`', function() {
+  it('finds `changelog.md` and `CHANGELOG.md`', function(done) {
 
     files = [{
       name: 'CHANGELOG.md'
@@ -82,11 +89,12 @@ describe('Parse repo contents', function() {
 
     gitParse(null, files, function(result) {
       expect(result.hasChangelog).to.be(true);
+      done();
     });
 
   });
 
-  it('finds thumbnail named `ofxaddons_thumbnail.png`', function() {
+  it('finds thumbnail named `ofxaddons_thumbnail.png`', function(done) {
 
     files = [{
       name: 'ofxaddons_thumbnail.png'
@@ -94,10 +102,27 @@ describe('Parse repo contents', function() {
 
     gitParse(null, files, function(result) {
       expect(result.hasThumbnail).to.be(true);
+      done();
     });
 
   });
+});
 
+describe('Call after given time', function() {
 
+  it('calls function with params given future timestamp', function(done) {
+
+    var timeout = Math.round(+new Date() / 1000) + 1; // 1 second from now
+
+    callAfter(timeout, function(one, two, three) {
+      var timeNow = Math.round(+new Date() / 1000);
+      expect(timeNow).to.be(timeout);
+      expect(one).to.be('one');
+      expect(two).to.be('two');
+      expect(three).to.be('three');
+      done();
+    }, ['one', 'two', 'three'])
+
+  });
 
 });
