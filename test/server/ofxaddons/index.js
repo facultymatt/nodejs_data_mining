c45f0c122497ee9b8e07f5ce19f0b61cb3314725
Matt Miller <matt@facultycreative.com>
2014-05-13 22:32:33 -0400

    working tests and nocks.

--- test/server/ofxaddons/index.js
+++ test/server/ofxaddons/index.js
@@ -1,35 +1,235 @@
 'use strict';
 
 var expect = require('expect.js'),
-  express = require('express');
-  var nock = require('nock');
+  express = require('express'),
+  nock = require('nock'),
+  mocks = require('../ofxaddons/repoContentsMock.js'),
+  ofxGithub = require('../../../lib/ofxaddons/index.js');
 
-var mocks = require('../ofxaddons/repoContentsMock.js');
 
-var ofxGithub = require('../../../lib/ofxaddons/index.js');
+describe('--> getting repos contents', function() {
 
-describe('searching repos', function() {
+  var manyReposArray, singleRepoArray, limitArray, singleRepoObj, scope1, scope2, scope3, scopeLimit;
 
-  describe('on max request error from github', function() {
+  beforeEach(function(done) {
+    
+    scope1 = mocks.ofxAnimatable();
+    scope2 = mocks.ofxARDrone();
+    scope3 = mocks.ofxAudioUnit();
+    scopeLimit = mocks.requestLimit();
 
-    it('should retry after specified time', function(done) {
+    manyReposArray = [{
+      "full_name": "armadillu/ofxAnimatable"
+    }, {
+      "full_name": "memo/ofxARDrone"
+    }, {
+      "full_name": "admsyn/ofxAudioUnit"
+    }];
+    limitArray = [{
+      "full_name": "request/limit"
+    }];
+    singleRepoArray = [{
+      "full_name": "admsyn/ofxAudioUnit"
+    }];
+    singleRepoObj = {
+      "full_name": "admsyn/ofxAudioUnit"
+    };
+    done();
+  });
 
-      var demoArray = [{
-        "full_name": "armadillu/ofxAnimatable"
-      }, {
-        "full_name": "memo/ofxARDrone"
-      }, {
+  afterEach(function(done) {
+    nock.cleanAll();
+    done();
+  });
+
+  describe('given array of repo objects with at least full_name property', function() {
+
+    it('will perform async lookup to github for each repo', function(done) {
+      ofxGithub.getDetailsForRepoArrayAsync(manyReposArray, function(reposWithDetails) {
+        expect(reposWithDetails.length).to.be(3);
+        scope1.done();
+        scope2.done();
+        scope3.done();
+        done();
+      }, function(err) {
+        if (err) throw err;
+      });
+    });
+
+  });
+
+  describe('given array of one repo', function() {
+
+    it('will lookup details for single repo', function(done) {
+      ofxGithub.getDetailsForRepoArrayAsync(singleRepoArray, function(repoWithDetails) {
+        expect(repoWithDetails.length).to.be(1);
+        scope3.done();
+        done();
+      }, function(err) {
+        if (err) throw err;
+      });
+    });
+
+  });
+
+  describe('given a single repo object', function() {
+
+    it('will lookup details for single repo object', function(done) {
+
+      ofxGithub.getDetailsForRepoArrayAsync(singleRepoObj, function(repoWithDetails) {
+        expect(repoWithDetails.length).to.be(1);
+        scope3.done();
+        done();
+      }, function(err) {
+        if (err) throw err;
+      });
+    });
+
+  });
+
+  describe('on API rate limit exceeded error from github', function() {
+
+    it('should retry after `x-ratelimit-reset` specified in header', function(done) {
+
+      // get details for this repo! 
+      ofxGithub.getDetailsForRepoArrayAsync(limitArray, function() {
+        // if timeout is handled properly by crawler this callback will
+        // never happen. No need to check contents of callback here. 
+        scopeLimit.done();
+        done();
+      }, function(err) {
+        if (err) throw err;
+      });
+
+    });
+
+  });
+
+});
+
+
+describe('--> parsing file contents', function() {
+
+  var authParams = '&client_id=949b776f9fc88851972e&client_secret=4793f09a0189098c72bcded04945c65d0f3a3001';
+
+  var parsedContents, addon;
+
+  afterEach(function(done) {
+    nock.cleanAll();
+    done();
+  });
+
+  describe('repo with no important files', function(done) {
+
+    var scope;
+
+    beforeEach(function(done) {
+
+      scope = mocks.lame();
+
+      addon = {
+        "full_name": "lame/addon"
+      };
+
+      parsedContents = {
+        full_name: "lame/addon",
+        has_makefile: false,
+        has_thumbnail: false,
+        has_readme: false,
+        has_changelog: false,
+        num_examples: 0
+      };
+
+      done();
+
+    });
+
+    it('should return default object', function(done) {
+
+      //mocks.lame();
+
+      ofxGithub.getDetailsForRepoArrayAsync(addon, function(repoWithDetails) {
+        expect(repoWithDetails[0]).to.eql(parsedContents);
+        done();
+      }, function(err) {
+        if (err) throw err;
+      });
+
+    });
+
+  });
+
+  describe('repo with many important files!', function(done) {
+
+    var scope;
+
+    beforeEach(function(done) {
+
+      scope = mocks.ofxAudioUnit();
+
+      addon = {
         "full_name": "admsyn/ofxAudioUnit"
-      }];
+      };
 
-      mocks.limit('/repos/armadillu/ofxAnimatable/contents/?ref=master');
+      parsedContents = {
+        full_name: "admsyn/ofxAudioUnit",
+        has_makefile: true,
+        has_thumbnail: true,
+        has_readme: true,
+        has_changelog: true,
+        num_examples: 5
+      };
 
-      ofxGithub.getDetailsForRepoArrayAsync(demoArray, function(result) {
-        //console.log(result);
+      done();
+
+    });
+
+    it('should return default object', function(done) {
+
+      ofxGithub.getDetailsForRepoArrayAsync(addon, function(repoWithDetails) {
+        expect(repoWithDetails[0]).to.eql(parsedContents);
         done();
-      }, function(error) {
-        //console.log(result);
+      }, function(err) {
+        if (err) throw err;
+      });
+
+    });
+
+  });
+
+  describe('repo with alternate makefile', function(done) {
+
+    var scope;
+
+    beforeEach(function(done) {
+
+      scope = mocks.altMakeFile();
+
+      addon = {
+        "full_name": "alt/makefile"
+      };
+
+      parsedContents = {
+        full_name: "alt/makefile",
+        has_makefile: true,
+        has_thumbnail: false,
+        has_readme: false,
+        has_changelog: false,
+        num_examples: 0
+      };
+
+      done();
+
+    });
+
+    it('should return default object', function(done) {
+
+      ofxGithub.getDetailsForRepoArrayAsync(addon, function(repoWithDetails) {
+        expect(repoWithDetails[0]).to.eql(parsedContents);
+        scope.done();
         done();
+      }, function(err) {
+        if (err) throw err;
       });
 
     });
